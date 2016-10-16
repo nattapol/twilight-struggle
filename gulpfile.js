1d79e202c1de7052317a6d8fda8ec863b2e29843
Nattapol Kengkuntod <nattapol.ke@gmail.com>
2016-10-16 18:31:15 +0700

    Finish gulpfile init

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,27 @@
 var gulp 		= require('gulp');
-var browserSync = require('browser-sync').create();
 var sass		= require('gulp-ruby-sass');
+var browserSync	= require('browser-sync');
 
-// Static Server + watching scss/html files
-gulp.task('server', ['sass'], function() {
-	browserSync.init({
-		server: "./"
-	});
-
-	gulp.watch("/css/*.scss", ['sass']);
-	gulp.watch("app/*.html").on('change', browserSync.reload);
+gulp.task('sass', function () {
+	return sass('scss/styles.scss', {
+			compass: true,
+			style: 'compressed'
+		})
+		.pipe(gulp.dest('css'));
 });
 
-/**
- * Complie with gulp-ruby-sass + source maps
- */
-gulp.task('sass', function () {
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: "./"
+		}
+	});
+});
 
-	return sass('css', {sourcemap: true})
-		.on('error', function (err) {
-			console.error('Error!', err.message);
-		})
-		.pipe(sourcemaps.write('./', {
-			includeContent: false,
-			sourceRoot: 'css'
-		}))
+gulp.task('default', ['sass', 'browser-sync'], function() {
+	
+	gulp.watch(['**/*.html'], browserSync.reload);
+	gulp.watch(['css/styles.css'], browserSync.reload);
+
+	gulp.watch("scss/**/*.scss", ['sass']);
 });
